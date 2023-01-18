b7d7d359d3776375f6fa66b158854c2bc5e754bb
vasile.razdalovschi <vasile.razdalovschi@gmail.com>
2023-01-18 16:18:34 +0200

    implement expiration for get/set command

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 # Uncomment this to pass the first stage
 import socket
 import _thread
+from datetime import datetime
 
 from app.resp_decoder import RESPDecoder
 
@@ -11,15 +12,33 @@ def handle_client(client_connection):
     while True:
         try:
             command, *args = RESPDecoder(client_connection).decode()
+            print(command, args)
             if command == b"ping":
                 client_connection.send(b"+PONG\r\n")
             elif command == b"echo":
                 client_connection.send(b"$%d\r\n%b\r\n" % (len(args[0]), args[0]))
             elif command == b"set":
-                memory[args[0]] = args[1]
+                store_result = (args[1], None)
+                if len(args) == 4 and args[2] == b"px":
+                    now = datetime.now().timestamp()
+                    exp_at = now + int(args[3].decode()) / 1000
+                    store_result = (args[1], exp_at)
+
+                memory[args[0]] = store_result
                 client_connection.send(b"+OK\r\n")
             elif command == b"get":
-                client_connection.send(b"$%d\r\n%b\r\n" % (len(memory[args[0]]), memory[args[0]]))
+                res, exp = memory[args[0]]
+                if not res:
+                    client_connection.send(b"$-1\r\n")
+                    continue
+
+                now = datetime.now().timestamp()
+                if exp and now > exp:
+                    memory[args[0]] = None
+                    client_connection.send(b"$-1\r\n")
+                    continue
+
+                client_connection.send(b"$%d\r\n%b\r\n" % (len(res), res))
             else:
                 client_connection.send(b"-ERR unknown command\r\n")
         except ConnectionError:
